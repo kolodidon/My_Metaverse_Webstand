8a8d42f128a52b1dff59869f0467f118ef34e6dd
kolodidon <www.teteva@yandex.ru>
2023-06-23 12:04:50 +0300

    Setting up the project display on github_pages.

---
+++ _static/_next/static/9y7LHVViIHrP8XYAJEibs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-59166ac763ce8d4d.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
