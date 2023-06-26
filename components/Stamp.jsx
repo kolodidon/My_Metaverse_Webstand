37d0528d7c0c583018e0361af1fdfc219b7beca8
kolodidon <www.teteva@yandex.ru>
2023-06-26 13:55:18 +0300

    Linter code styling fixes

--- components/Stamp.jsx
+++ components/Stamp.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 import { motion } from 'framer-motion';
 
 import { stampHoverVariant, fadeIn } from '../utils/motion';
 
-const Stamp = ({ idx, href, bgColor, textColor, subTitle }) => {
-    return (
-        <motion.a
-            href={href}
-            whileHover={stampHoverVariant}
-            variants={fadeIn('right', 'spring', idx * 0.75, 1)}
-        >
-            <div 
-                style={{ backgroundColor: bgColor, color: textColor, }}
-                className={`p-[15px] w-40 h-28 flex flex-col rounded-3xl rounded-tr-none`}
-            >
-                <span className="text-5xl font-bold tracking-tighter leading-[1.2]">
-                    Sk
-                </span>
-                <span>
-                    {subTitle}
-                </span>
-            </div>
-        </motion.a>
-    )
-}
+const Stamp = ({ idx, href, bgColor, textColor, subTitle }) => (
+  <motion.a
+    href={href}
+    whileHover={stampHoverVariant}
+    variants={fadeIn('right', 'spring', idx * 0.75, 1)}
+  >
+    <div
+      style={{ backgroundColor: bgColor, color: textColor }}
+      className="p-[15px] w-40 h-28 flex flex-col rounded-3xl rounded-tr-none"
+    >
+      <span className="text-5xl font-bold tracking-tighter leading-[1.2]">
+        Sk
+      </span>
+      <span>
+        {subTitle}
+      </span>
+    </div>
+  </motion.a>
+);
 
-export default Stamp
+export default Stamp;
